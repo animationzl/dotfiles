f7c2e5853563e7df1078cd1b22a2abaa452fdcee
zhuli <zhuli27@huawei.com>
2018-02-06 02:18:51 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TOP_DIR=$(cd $(dirname "$0") && pwd)
+
+# install tpope/vim-pathogen
+if [[ ! -e ~/.vim/autoload/pathogen.vim ]]; then
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+# install plugins
+for repo in `cat $TOP_DIR/plugins.vim`
+do
+    short_name=${repo##*/}
+    if [[ ! -d ~/.vim/bundle/$short_name ]]; then
+        git clone https://github.com/$repo ~/.vim/bundle/$short_name
+    fi
+done
+
+if [[ ! -e ~/.vimrc ]]; then
+    cp $TOP_DIR/.vimrc ~/.vimrc
+fi
+
+if [[ ! -e ~/.tmux.conf ]]; then
+    cp $TOP_DIR/.tmux.conf ~/.tmux.conf
+fi
