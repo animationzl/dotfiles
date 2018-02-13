0e115a13d6e496d3726f24332946b571dbbf96bb
zhuli <zhuli27@huawei.com>
2018-02-13 18:46:00 +0800

    Fine tune file name

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 
 TOP_DIR=$(cd $(dirname "$0") && pwd)
 
-# install tpope/vim-pathogen
+# Install tpope/vim-pathogen
 if [[ ! -e ~/.vim/autoload/pathogen.vim ]]; then
     mkdir -p ~/.vim/autoload ~/.vim/bundle && \
         curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
-# install plugins
+# Install vim plugins
 while read repo
 do
     short_name=${repo##*/}
     if [[ ! -d ~/.vim/bundle/$short_name ]]; then
         git clone https://github.com/$repo ~/.vim/bundle/$short_name
     fi
-done < $TOP_DIR/plugins.vim
+done < $TOP_DIR/vim-plugins
 
 if [[ ! -e ~/.vimrc ]]; then
     cp $TOP_DIR/.vimrc ~/.vimrc
