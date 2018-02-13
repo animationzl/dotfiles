260fb1c329a2af3d6141893cbb73be7f15f4eb15
zhuli <zhuli27@huawei.com>
2018-02-14 01:00:57 +0800

    Add dependent packages and shell aliases

--- install.sh
+++ install.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-TOP_DIR=$(cd $(dirname "$0") && pwd)
+cd $(dirname "$0")
 
-# Install tpope/vim-pathogen
-if [[ ! -e ~/.vim/autoload/pathogen.vim ]]; then
-    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-        curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-fi
-
-# Install vim plugins
-while read repo
+# Install packages
+while read pkg
 do
-    short_name=${repo##*/}
-    if [[ ! -d ~/.vim/bundle/$short_name ]]; then
-        git clone https://github.com/$repo ~/.vim/bundle/$short_name
-    fi
-done < $TOP_DIR/vim-plugins
+    apt install -y $pkg
+done < apt-packages
 
-if [[ ! -e ~/.vimrc ]]; then
-    cp $TOP_DIR/.vimrc ~/.vimrc
+if [[ ! -e ~/.zshrc ]]; then
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    { echo; cat shell-aliases; } >> ~/.zshrc
+    chsh -s $(which zsh)
 fi
 
 if [[ ! -e ~/.tmux.conf ]]; then
-    cp $TOP_DIR/.tmux.conf ~/.tmux.conf
+    cp .tmux.conf ~/.tmux.conf
+fi
+
+if [[ ! -e ~/.vimrc ]]; then
+    cp .vimrc ~/.vimrc
+
+    # Install tpope/vim-pathogen
+    if [[ ! -e ~/.vim/autoload/pathogen.vim ]]; then
+        mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+            curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    fi
+
+    # Install vim plugins
+    while read repo
+    do
+        short_name=${repo##*/}
+        if [[ ! -d ~/.vim/bundle/$short_name ]]; then
+            git clone https://github.com/$repo ~/.vim/bundle/$short_name
+        fi
+    done < vim-plugins
 fi
