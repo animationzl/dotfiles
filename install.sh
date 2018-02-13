61ce7c3ad0aba1d8367c0827383e308c4db5384d
zhuli <zhuli27@huawei.com>
2018-02-13 18:18:19 +0800

    Optimization

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ TOP_DIR=$(cd $(dirname "$0") && pwd)
 # install tpope/vim-pathogen
 if [[ ! -e ~/.vim/autoload/pathogen.vim ]]; then
     mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+        curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
 # install plugins
-for repo in `cat $TOP_DIR/plugins.vim`
+while read repo
 do
     short_name=${repo##*/}
     if [[ ! -d ~/.vim/bundle/$short_name ]]; then
         git clone https://github.com/$repo ~/.vim/bundle/$short_name
     fi
-done
+done < $TOP_DIR/plugins.vim
 
 if [[ ! -e ~/.vimrc ]]; then
     cp $TOP_DIR/.vimrc ~/.vimrc
