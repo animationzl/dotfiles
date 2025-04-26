68be69c63eb76681264889363ba11fc9b4c4db8a
sethzhu <sethzhu@tencent.com>
2025-04-26 22:25:06 +0800

    nvim: support mouse hover

---
+++ .config/nvim/lua/plugins/hover.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "lewis6991/hover.nvim",
+    config = function()
+      require("hover").setup({
+        init = function()
+          -- Require providers
+          require("hover.providers.lsp")
+        end,
+        preview_opts = {
+          border = "single",
+        },
+        -- Whether the contents of a currently open hover window should be moved
+        -- to a :h preview-window when pressing the hover keymap.
+        preview_window = false,
+        title = false,
+        mouse_providers = {
+          "LSP",
+        },
+        mouse_delay = 500,
+      })
+
+      -- Mouse support
+      vim.keymap.set("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
+      vim.o.mousemoveevent = true
+    end,
+  },
+}
