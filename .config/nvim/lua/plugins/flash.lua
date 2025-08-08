0396dd6eded1612a67d0696341edefa6f28cf8eb
sethzhu <sethzhu@tencent.com>
2025-08-08 16:08:18 +0800

    nvim: enable jump labels for the char mode

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    modes = {
+      char = {
+        jump_labels = true, -- Show jump labels for character motions
+        multi_line = false, -- Restrict f/t/F/T to single-line jumps by default
+      },
+    },
+  },
+}
